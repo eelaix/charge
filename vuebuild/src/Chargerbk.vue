0c0696e106c32a15edfc66aae69744c6032020fd
eelaix <eelaix@gmail.com>
2024-01-01 17:07:12 +0800

    ok

--- vuebuild/src/Chargerbk.vue
+++ vuebuild/src/Chargerbk.vue
@@ -237,14 +237,16 @@
       paystack
     },
     mounted() {
+      while (!this.ayoba_msisdn) {
         this.ayoba_msisdn = getUserPhoneNumber();
-        this.ayoba_selfjid = getURLParameter('jid');
-        let avatarcallback = (avatar) => {this.ayoba_avatar = avatar;};
-        let onlinecallback = (online) => {this.ayoba_presence = online;};
-        let usernamecallback = (username) => {this.ayoba_nickname = username;};
-        getUserAvatar(avatarcallback);
-        getUserName(usernamecallback);
-        observeUserPresence(onlinecallback);
+      }
+      this.ayoba_selfjid = getURLParameter('jid');
+      let avatarcallback = (avatar) => {this.ayoba_avatar = avatar;};
+      let onlinecallback = (online) => {this.ayoba_presence = online;};
+      let usernamecallback = (username) => {this.ayoba_nickname = username;};
+      getUserAvatar(avatarcallback);
+      getUserName(usernamecallback);
+      observeUserPresence(onlinecallback);
       this.fetchData();
     },
     computed: {
