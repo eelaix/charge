e73185ff7debdf4d0171145dce11da2aa2d883cd
eelaix <eelaix@gmail.com>
2024-01-15 16:35:52 +0800

    ok

--- vuebuild/go.sh
+++ vuebuild/go.sh
@@ -4,5 +4,5 @@ echo  npm install --save
 echo  npm run serve
 echo  npm run build -- --dest=../docs
 echo  cp -rf dist/* ..
-echo  npm install --save
+npm install --save
 npm run build -- --dest=../docs
