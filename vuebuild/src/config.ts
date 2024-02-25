acfc69ded08bc5e60ad3fe63b33d8787b8424e4e
eelaix <eelaix@gmail.com>
2024-02-25 15:38:29 +0800

    Update config.ts

--- vuebuild/src/config.ts
+++ vuebuild/src/config.ts
@@ -12,7 +12,7 @@ export const prepaylimit: number = 100
 function padTo2Digits(num: number): string {
   return num.toString().padStart(2, '0')
 }
-export const formatDate = function (date: Date) {
+export const formatDate = function (date: Date): string {
   return (
     [date.getFullYear(), padTo2Digits(date.getMonth() + 1), padTo2Digits(date.getDate())].join(
       '-'
@@ -25,6 +25,6 @@ export const formatDate = function (date: Date) {
     ].join(':')
   )
 }
-export const getAssetsFile = function (url: string) {
+export const getAssetsFile = function (url: string): string {
   return new URL(`./assets/images/${url}`, import.meta.url).href
 }
